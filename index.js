02efe5b92e8edf9a7ab43e5931b7f06aa0ad2499
Nhat Nguyen <islocated@gmail.com>
2020-03-26 11:48:42 -0500

    Set up express server with socket.io using ES6

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import http from 'http'
+import SocketIO from 'socket.io'
+
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const server = http.Server(app);
+const io = new SocketIO(server);
+
+
+app.get('/', (req, res) => {
+    //res.send('Hello World');
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+server.listen(8000, () => {
+    console.log('Example app listening on port 8000');
+});
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);   //send to everyone
+        //socket.broadcast.emit('hi');    //send to everyone else
+    })
+});
